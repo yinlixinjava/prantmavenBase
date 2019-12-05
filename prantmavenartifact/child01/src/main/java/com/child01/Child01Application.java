90887f2c9bc3ea58a19720481abdf886492a074b
yinlixinjava <13269755565@163.com>
2019-12-05 17:17:31 +0800

    第一个版本,下午五点提交的,新的仓库

---
+++ prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
@@ -0,0 +1,13 @@
+package com.child01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Child01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Child01Application.class, args);
+	}
+
+}
---
+++ prantmavenartifact/child02/src/main/java/com/child02/Child02Application.java
@@ -0,0 +1,12 @@
+package com.child02;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Child02Application {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Child02Application.class, args);
+	}
+
+}
