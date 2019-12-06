edc57d9ac2d99469f4739e2f4c82aa415f9555d6
yinlixinjava <13269755565@163.com>
2019-12-06 16:00:39 +0800

    没有更新就写自己代码,之后提交了

--- prantmavenartifact/child01/src/main/java/com/package01/DeleteMain.java
+++ prantmavenartifact/child01/src/main/java/com/package01/DeleteMain.java
@@ -5,6 +5,12 @@ package com.package01;
  */
 public class DeleteMain {
     public static void main(String[] args) {
-        System.out.println("先提交后删除01");
+        System.out.println("先提交后删除101");
+        System.out.println("先提交后删除201");
+        System.out.println("先提交后删除301");
+        System.out.println("先提交后删除401====================");
+        System.out.println("先提交后删除501");
+        System.out.println("先提交后删除601");
+        System.out.println("先提交后删除701");
     }
 }
