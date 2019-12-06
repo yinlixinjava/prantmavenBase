2c3388293d723386c6a5da8b69c3a7c5bac58016
yinlixinjava <13269755565@163.com>
2019-12-06 10:27:20 +0800

    加了一个2255

--- prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
+++ prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
@@ -9,6 +9,7 @@ public class Child01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Child01Application.class, args);
 		System.out.println(1155);
+		System.out.println(2255);
 	}
 
 }
