f1d224dc92fa6c9bcd696878de08b25c317ea1a1
yinlixinjava <13269755565@163.com>
2019-12-06 12:06:39 +0800

    增加00和0101

--- prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
+++ prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
@@ -8,8 +8,9 @@ public class Child01Application {
 	public static void main(String[] args) {
 		//SpringApplication.run(Child01Application.class, args);
 
-
+		System.out.println("=====================00====================");
 		System.out.println("=====================01====================");
+		System.out.println("=====================0101====================");
 		System.out.println("=====================02====================");
 	}
 
