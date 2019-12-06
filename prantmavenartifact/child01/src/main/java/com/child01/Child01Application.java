d31991e6e600643f7bedd85f4d24f8cbeaf045f8
yinlixinjava <13269755565@163.com>
2019-12-06 11:55:07 +0800

    测试冲突增加01和02

--- prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
+++ prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
@@ -1,15 +1,16 @@
 package com.child01;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Child01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Child01Application.class, args);
-		System.out.println(1155);
-		System.out.println(2255);
+		//SpringApplication.run(Child01Application.class, args);
+
+
+		System.out.println("=====================01====================");
+		System.out.println("=====================02====================");
 	}
 
 }
