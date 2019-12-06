91ded1cd25090849fda2b16193ab033fa689b56c
yinlixinjava <13269755565@163.com>
2019-12-06 12:00:28 +0800

    ye增加01和02

---
+++ prantmavenartifact/child01/src/main/java/com/package01/Package01Main01.java
@@ -0,0 +1,11 @@
+package com.package01;
+
+/**
+ * Created by admin on 2019/12/6.
+ */
+public class Package01Main01 {
+    public static void main(String[] args) {
+        System.out.println("-------01------");
+        System.out.println("-------02------");
+    }
+}
