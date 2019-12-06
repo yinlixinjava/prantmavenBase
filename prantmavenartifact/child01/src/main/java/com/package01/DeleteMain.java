284b2f154366918ab39280a7dedca016cd705d13
yinlixinjava <13269755565@163.com>
2019-12-06 15:51:02 +0800

    提交后再pull

--- prantmavenartifact/child01/src/main/java/com/package01/DeleteMain.java
+++ prantmavenartifact/child01/src/main/java/com/package01/DeleteMain.java
@@ -5,6 +5,8 @@ package com.package01;
  */
 public class DeleteMain {
     public static void main(String[] args) {
+        System.out.println("先提交后删除,修改着的文件在pull,看会不会被覆盖00000000000000000000");
         System.out.println("先提交后删除01");
+        System.out.println("先提交后删除,修改着的文件在pull,看会不会被覆盖111111111111111111111");
     }
 }
