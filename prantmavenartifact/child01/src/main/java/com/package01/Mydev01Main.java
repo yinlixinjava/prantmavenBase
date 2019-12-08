b90600ea363b70c13c3f0e6784a55700c6015bd5
13269755565 <yinlixin123>
2019-12-08 16:40:14 +0800

    我是mydev01分支01

---
+++ prantmavenartifact/child01/src/main/java/com/package01/Mydev01Main.java
@@ -0,0 +1,10 @@
+package com.package01;
+
+/**
+ * Created by 13269755565 on 2019/12/8.
+ */
+public class Mydev01Main {
+    public static void main(String[] args) {
+        System.out.println("我是mydev01分支01");
+    }
+}
