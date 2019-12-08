c88f87d6a58a15aaf2e57d29962838741f462a90
13269755565 <yinlixin123>
2019-12-08 17:20:15 +0800

    我改为mydev0101

--- prantmavenartifact/child01/src/main/java/com/package01/Mydev01Main.java
+++ prantmavenartifact/child01/src/main/java/com/package01/Mydev01Main.java
@@ -6,5 +6,6 @@ package com.package01;
 public class Mydev01Main {
     public static void main(String[] args) {
         System.out.println("我是mydev01分支01");
+        System.out.println("我将mydev01分支改为mydev0101，提交到远程");
     }
 }
