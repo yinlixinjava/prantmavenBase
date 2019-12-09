defbfa67f894846aef72fa48a6b36460655ec317
985912628 <58681932+985912628@users.noreply.github.com>
2019-12-09 18:14:58 +0800

    Update MergeTest02.java
    System.out.println("985912628新增的");

--- prantmavenartifact/child01/src/main/java/com/child01/MergeTest02.java
+++ prantmavenartifact/child01/src/main/java/com/child01/MergeTest02.java
@@ -7,5 +7,6 @@ public class MergeTest02 {
     public static void main(String[] args) {
         System.out.println("拿新文件测试下02");
         System.out.println("985912628新增的");
+        System.out.println("不勾选");
     }
 }
