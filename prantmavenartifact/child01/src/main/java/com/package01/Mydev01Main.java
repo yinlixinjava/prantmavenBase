d4b50f98701018a167330858e5a8a3ddce1bafdd
yinlixinjava <13269755565@163.com>
2019-12-09 10:46:00 +0800

    System.out.println("我在公司建立本地的mydev01分支,并且和服务器上的mydev01关联");

--- prantmavenartifact/child01/src/main/java/com/package01/Mydev01Main.java
+++ prantmavenartifact/child01/src/main/java/com/package01/Mydev01Main.java
@@ -7,5 +7,6 @@ public class Mydev01Main {
     public static void main(String[] args) {
         System.out.println("我是mydev01分支01");
         System.out.println("我将mydev01分支改为mydev0101，提交到远程");
+        System.out.println("我在公司建立本地的mydev01分支,并且和服务器上的mydev01关联");
     }
 }
