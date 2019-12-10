00672898fdfaf33eae72cc4c9088f935a721c91c
yinlixinjava <13269755565@163.com>
2019-12-10 10:29:15 +0800

    之前出现的不一致问题

--- prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
+++ prantmavenartifact/child01/src/main/java/com/child01/Child01Application.java
@@ -14,15 +14,15 @@ public class Child01Application {
 		System.out.println("=====================02.01====================");
 		System.out.println("试一下修改多个文件试试02=====");
 
-        System.out.println("刚才发生了冲突,这次我想重新试一下02");
+		System.out.println("刚才发生了冲突,这次我想重新试一下02");
 
 
 		System.out.println("这次我真的发现问题了,是我选错要merge的分支了02");
-        System.out.println("希望这次是对的02");
+		System.out.println("希望这次是对的02");
 
 
-        System.out.println("下午第一次,先什么都不选02");
-        System.out.println("在试一下选择第二个是什么鬼02");
+		System.out.println("下午第一次,先什么都不选02");
+		System.out.println("在试一下选择第二个是什么鬼02");
 		System.out.println("我是985912628,我想试试merge选择第二个");
 		System.out.println("单独测试下");
 		System.out.println("单独测试下222");
